d2ba6cc821d14aacb94a71c90c4299d8f9ddf5d1
Phelipe Teles <phelipe_teles@hotmail.com>
2021-04-26 19:39:54 -0300

    Try to reduce flash of wrong theme
    By putting script right after body tag

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,36 +4,15 @@ function toTitleCase(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
-function setTheme(newTheme) {
-  document.body.setAttribute("data-theme", newTheme);
-  localStorage.setItem("__theme", newTheme);
-
-  const oldTheme = newTheme === "dark" ? "light" : "dark";
-
-  document
-    .querySelector("link[href*='" + oldTheme + "']")
-    .setAttribute("disabled", "");
-
-  document
-    .querySelector("link[href*='" + newTheme + "']")
-    .removeAttribute("disabled");
-
-  button.textContent = toTitleCase(newTheme);
-}
-
-const storedTheme = localStorage.getItem("__theme");
-
-if (storedTheme) {
-  setTheme(storedTheme);
-} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  setTheme("dark");
-}
-
 button.addEventListener("click", function() {
   if (document.body.dataset.theme === "dark") {
-    setTheme("light");
+    window.__setTheme("light");
     return;
   }
 
-  setTheme("dark");
+  window.__setTheme("dark");
+});
+
+button.addEventListener("themeChange", function(e) {
+  e.target.textContent = toTitleCase(e.detail.theme);
 });

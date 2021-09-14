dc89cbe69296c06cff960b4624b631b9439d4206
Phelipe Teles <phelipe_teles@hotmail.com>
2021-09-13 23:01:12 -0300

    Configure tailwind colors and use them

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: 'var(--primary)',
+      highlight: 'var(--highlight)',
+      divider: 'var(--divider)',
+      normal: {
+        DEFAULT: 'var(--normal)',
+        code: 'var(--normal-code)',
+      },
+      background: {
+        DEFAULT: 'var(--background)',
+        code: 'var(--background-code)',
+      }
+    },
   },
   variants: {
     extend: {

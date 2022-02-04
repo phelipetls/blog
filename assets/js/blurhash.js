52f2d0a147c5c5f906fccd7e8c630e8676fd4704
Phelipe Teles <phelipe_teles@hotmail.com>
2022-02-04 10:22:32 -0300

    Don't use Hugo's ExecuteAsTemplate to fill blurhash value
    Use a data attribute instead

--- assets/js/blurhash.js
+++ assets/js/blurhash.js
@@ -1,12 +1,10 @@
 import { decode } from "blurhash";
 
-const width = 200;
-const height = 200;
-
-const pixels = decode("{{ .Blurhash }}", width, height);
-
 const canvas = document.getElementById("gravatar-blurhash");
 const ctx = canvas.getContext("2d");
-const imageData = ctx.createImageData(width, height);
+
+const pixels = decode(canvas.dataset.blurhash, canvas.width, canvas.height);
+
+const imageData = ctx.createImageData(canvas.width, canvas.height);
 imageData.data.set(pixels);
 ctx.putImageData(imageData, 0, 0);

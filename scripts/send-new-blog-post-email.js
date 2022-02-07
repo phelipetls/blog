74d9fd14c03a3346f954f8b29b3b074f2eaed1b7
Phelipe Teles <phelipe_teles@hotmail.com>
2022-02-07 15:19:21 -0300

    Exit succesfully if error was sending a duplicate email

--- scripts/send-new-blog-post-email.js
+++ scripts/send-new-blog-post-email.js
@@ -81,6 +81,11 @@ const BUTTONDOWN_API_KEY = process.env.BUTTONDOWN_API_KEY;
     if (!response.ok) {
       const text = await response.text();
       console.error(`[${response.status}] Could not send email: \n${text}`);
+
+      if (response.status === 400 && text === 'You have already sent an email with an identical subject and body.') {
+        process.exit(0);
+      }
+
       process.exit(1);
     }
 

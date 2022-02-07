8684bb1cd82129f134a12c38086e2868289a9d5c
Phelipe Teles <phelipe_teles@hotmail.com>
2022-02-07 10:19:17 -0300

    Add node script to send email with Buttondown API
    This is how it works
    
    We get two blog posts RSS feeds, one from the local build and one from
    the production build.
    
    The goals it to get the blog posts that are yet to be published, meaning
    the posts not in the production RSS feed.
    
    So we use the library rss-parser to parse the RSS feeds and get the
    difference betwen the items.
    
    Then we use Buttondown API to send the email.
    
    The content of the email (body) is built with Hugo using a custom output
    format named email.

---
+++ scripts/send-new-blog-post-email.js
@@ -0,0 +1,89 @@
+const RssParser = require("rss-parser");
+const path = require("path");
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+function diffXmlFeedItems(newXml, oldXml) {
+  const oldXmlItems = oldXml.items.map((item) => item.guid);
+
+  const newXmlItems = newXml.items.filter(
+    (item) => !oldXmlItems.includes(item.guid)
+  );
+
+  return newXmlItems;
+}
+
+async function getLocalXmlFeed() {
+  const localXmlFeedPath = path.join(__dirname, "../public/posts/index.xml");
+  const localXmlFeed = fs.readFileSync(localXmlFeedPath);
+
+  const parser = new RssParser();
+  return parser.parseString(localXmlFeed);
+}
+
+async function getDeployedXmlFeed() {
+  const parser = new RssParser();
+  const url = "https://phelipetls.github.io/posts/index.xml";
+  return parser.parseURL(url);
+}
+
+async function getNewXmlFeedItems() {
+  const localXmlFeed = await getLocalXmlFeed();
+  const deployedXmlFeed = await getDeployedXmlFeed();
+
+  return diffXmlFeedItems(localXmlFeed, deployedXmlFeed);
+}
+
+async function sendEmail(body) {
+  const createEmailUrl = BUTTONDOWN_BASE_URL + "/v1/emails";
+
+  return fetch(createEmailUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+    },
+    body: JSON.stringify(body),
+  });
+}
+
+const BUTTONDOWN_BASE_URL = "https://api.buttondown.email";
+const BUTTONDOWN_API_KEY = process.env.BUTTONDOWN_API_KEY;
+
+(async () => {
+  const newPosts = await getNewXmlFeedItems();
+
+  if (newPosts.length === 0) {
+    console.log("No new posts. Nothing to do.");
+    process.exit(0);
+  }
+
+  for (const newPost of newPosts) {
+    const url = new URL(newPost.link);
+
+    const bodyPath = path.join(
+      __dirname,
+      "..",
+      "/public",
+      "email",
+      url.pathname,
+      "index.md"
+    );
+
+    const body = fs.readFileSync(bodyPath, "utf8");
+
+    const response = await sendEmail({
+      subject: newPost.title + " - phelipetls.github.io",
+      body,
+      external_url: newPost.link,
+    });
+
+    if (!response.ok) {
+      const text = await response.text();
+      console.error(`[${response.status}] Could not send email: \n${text}`);
+      process.exit(1);
+    }
+
+    console.log("Successfully sent email about new post:", newPost.title);
+  }
+})();

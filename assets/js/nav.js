217031cbb0efbac923a7e2d894389275a0b2866d
Phelipe Teles <phelipe_teles@hotmail.com>
2022-02-12 20:33:43 -0300

    Hide/show navbar when scrolling

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,26 @@
 const navContainer = document.querySelector(".nav-container");
 
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+let lastScrollDirection = 0;
+
+function handleScroll() {
+  const newScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  const scrollDirection = Math.sign(newScrollTop - lastScrollTop)
+
+  if (scrollDirection !== lastScrollDirection) {
+    if (scrollDirection === 1) {
+      navContainer.setAttribute('data-is-scrolling', 'down')
+    } else if (scrollDirection === -1) {
+      navContainer.setAttribute('data-is-scrolling', 'up')
+    }
+  }
+
+  lastScrollTop = Math.max(newScrollTop, 0)
+}
+
+window.addEventListener('scroll', handleScroll)
+
 const observer = new IntersectionObserver(
   function ([e]) {
     e.target.classList.toggle("stuck", e.intersectionRatio < 1);

748b7c10762b07413fd027e6586adae88c1a5763
Phelipe Teles <phelipe_teles@hotmail.com>
2022-04-10 17:31:22 -0300

    Refactor theme menu logic into a single file

---
+++ assets/js/menu.js
@@ -0,0 +1,230 @@
+// @ts-check
+import { computePosition, shift, offset } from '@floating-ui/dom'
+
+/**
+ * @type {(menu: HTMLElement) => NodeListOf<HTMLElement>}
+ */
+export function getMenuItems(menu) {
+  return menu.querySelectorAll('[role="menuitem"]')
+}
+
+/**
+ * @type {(menu: HTMLElement) => HTMLElement}
+ */
+function getActiveMenuItem(menu) {
+  let activeMenuItem
+
+  getMenuItems(menu).forEach(function (item) {
+    if (item.id === menu.getAttribute('aria-activedescendant')) {
+      activeMenuItem = item
+    }
+  })
+
+  return activeMenuItem
+}
+
+/**
+ * @type {(menu: HTMLElement) => HTMLElement}
+ */
+function getSelectedMenuItem(menu) {
+  let selectedMenuitem
+
+  getMenuItems(menu).forEach(function (item) {
+    if (item.getAttribute('aria-selected') === 'true') {
+      selectedMenuitem = item
+    }
+  })
+
+  return selectedMenuitem
+}
+
+/**
+ * @type {(menu: HTMLElement, targetItem: EventTarget) => void}
+ */
+function focusMenuItem(menu, targetItem) {
+  const focusedStyle = ['bg-surface']
+
+  getMenuItems(menu).forEach(function (item) {
+    if (item !== targetItem) {
+      item.classList.remove(...focusedStyle)
+    } else {
+      item.classList.add(...focusedStyle)
+      menu.setAttribute('aria-activedescendant', item.id)
+    }
+  })
+}
+
+/**
+ * @type {(menu: HTMLElement, targetItem: EventTarget) => void}
+ */
+function selectMenuItem(menu, targetItem) {
+  const selectedStyle = ['font-bold', 'text-primary']
+
+  getMenuItems(menu).forEach(function (item) {
+    if (item === targetItem) {
+      item.setAttribute('aria-selected', 'true')
+      item.classList.add(...selectedStyle)
+    } else {
+      item.removeAttribute('aria-selected')
+      item.classList.remove(...selectedStyle)
+    }
+  })
+}
+
+/**
+ * @type {(element: HTMLElement, duration: number) => void}
+ */
+function fadeIn(element, duration) {
+  element.style.transition = `visibility 0s linear, opacity ${duration}ms`
+  element.style.pointerEvents = 'auto'
+  element.style.opacity = '1'
+  element.style.visibility = 'visible'
+}
+
+/**
+ * @type {(element: HTMLElement, duration: number) => void}
+ */
+function fadeOut(element, duration) {
+  element.style.transition = `visibility 0s linear ${duration}ms, opacity ${duration}ms`
+  element.style.pointerEvents = 'none'
+  element.style.opacity = '0'
+  element.style.visibility = 'hidden'
+}
+
+/**
+ * @type {(menu: HTMLElement, button: HTMLButtonElement) => void}
+ */
+function showMenu(menu, button) {
+  button.setAttribute('aria-expanded', 'true')
+
+  fadeIn(menu, 500)
+
+  menu.setAttribute('aria-descendant', getSelectedMenuItem(menu).id)
+  focusMenuItem(menu, getSelectedMenuItem(menu))
+  menu.focus()
+}
+
+/**
+ * @type {(menu: HTMLElement, button: HTMLButtonElement) => void}
+ */
+function hideMenu(menu, button) {
+  fadeOut(menu, 500)
+
+  button.setAttribute('aria-expanded', 'false')
+  button.focus()
+
+  menu.removeAttribute('aria-activedescendant')
+}
+
+/**
+ * @typedef {object} InitializeOptions
+ * @property {(item: HTMLElement) => void} onClick
+ * @property {(item: HTMLElement) => boolean} isSelectedItem
+ */
+
+/**
+ * @type {(button: HTMLButtonElement, options: InitializeOptions) => void}
+ */
+export function initialize(button, options) {
+  const menu = document.getElementById(button.getAttribute('aria-controls'))
+
+  button.addEventListener('click', async function () {
+    if (button.getAttribute('aria-expanded') === 'true') {
+      hideMenu(menu, button)
+    } else {
+      showMenu(menu, button)
+
+      const { x, y } = await computePosition(button, menu, {
+        placement: 'bottom',
+        middleware: [offset(8), shift({ padding: 16 })],
+      })
+
+      Object.assign(menu.style, {
+        left: `${x}px`,
+        top: `${y}px`,
+      })
+    }
+  })
+
+  let hideMenuOnBlurTimeout
+
+  menu.addEventListener('focus', function () {})
+
+  menu.addEventListener('blur', function () {
+    hideMenuOnBlurTimeout = setTimeout(function () {
+      hideMenu(menu, button)
+    }, 300)
+  })
+
+  menu.addEventListener('click', function (e) {
+    if (options.onClick) {
+      options.onClick(getActiveMenuItem(menu))
+      selectMenuItem(menu, e.target)
+      hideMenu(menu, button)
+    }
+
+    clearTimeout(hideMenuOnBlurTimeout)
+  })
+
+  getMenuItems(menu).forEach(function (item) {
+    item.addEventListener('mouseover', function () {
+      focusMenuItem(menu, item)
+    })
+  })
+
+  getMenuItems(menu).forEach(function (item) {
+    if (
+      typeof options.isSelectedItem === 'function' &&
+      options.isSelectedItem(item)
+    ) {
+      selectMenuItem(menu, item)
+    }
+  })
+
+  menu.addEventListener('keydown', function (e) {
+    const activeItem = getActiveMenuItem(menu)
+
+    let nextActiveItem
+    let shouldPreventDefault
+
+    switch (e.key) {
+      case 'Escape':
+        hideMenu(menu, button)
+        shouldPreventDefault = true
+        break
+      case 'Enter':
+      case ' ':
+        if (options.onClick) {
+          options.onClick(getActiveMenuItem(menu))
+          selectMenuItem(menu, e.target)
+          hideMenu(menu, button)
+        }
+        shouldPreventDefault = true
+        break
+      case 'ArrowDown':
+        nextActiveItem = activeItem.nextElementSibling || menu.firstElementChild
+        break
+      case 'ArrowUp':
+        nextActiveItem =
+          activeItem.previousElementSibling || menu.lastElementChild
+        break
+      case 'Home':
+      case 'PageUp':
+        nextActiveItem = menu.firstElementChild
+        break
+      case 'End':
+      case 'PageDown':
+        nextActiveItem = menu.lastElementChild
+        break
+    }
+
+    if (nextActiveItem) {
+      focusMenuItem(menu, nextActiveItem)
+      shouldPreventDefault = true
+    }
+
+    if (shouldPreventDefault) {
+      e.preventDefault()
+    }
+  })
+}
--- assets/js/theme-button.js
+++ assets/js/theme-button.js
@@ -1,169 +1,19 @@
-import { computePosition, shift, offset } from '@floating-ui/dom'
+import { initialize } from './menu'
 
 const button = document.querySelector('[data-theme-button]')
-const menu = document.querySelector('[data-theme-menu]')
-const menuItems = menu.querySelectorAll('[role="menuitem"]')
-
-function getActiveMenuItem() {
-  for (const item of menuItems) {
-    if (item.id === menu.getAttribute('aria-activedescendant')) {
-      return item
-    }
-  }
-}
-
-function getSelectedMenuItem() {
-  for (const item of menuItems) {
-    if (item.getAttribute('aria-selected') === 'true') {
-      return item
-    }
-  }
-}
-
-function focusMenuItem(newItem) {
-  const focusedStyle = ['bg-surface']
-
-  menuItems.forEach(function (item) {
-    if (item !== newItem) {
-      item.classList.remove(...focusedStyle)
-    } else {
-      item.classList.add(...focusedStyle)
-      menu.setAttribute('aria-activedescendant', item.id)
-    }
-  })
-}
-
-function fadeIn(element, duration) {
-  element.style.transition = `visibility 0s linear, opacity ${duration}ms`
-  element.style.pointerEvents = 'auto'
-  element.style.opacity = 1
-  element.style.visibility = 'visible'
-}
-
-function fadeOut(element, duration) {
-  element.style.transition = `visibility 0s linear ${duration}ms, opacity ${duration}ms`
-  element.style.pointerEvents = 'none'
-  element.style.opacity = 0
-  element.style.visibility = 'hidden'
-}
-
-function showMenu() {
-  button.setAttribute('aria-expanded', 'true')
-
-  fadeIn(menu, 500)
-
-  menu.setAttribute('aria-descendant', getSelectedMenuItem().id)
-  focusMenuItem(getSelectedMenuItem())
-  menu.focus()
-}
-
-function hideMenu() {
-  fadeOut(menu, 500)
-
-  button.setAttribute('aria-expanded', 'false')
-  button.focus()
-
-  menu.removeAttribute('aria-activedescendant')
-}
 
 function setMenuItemTheme(item) {
-  hideMenu()
   window.__setTheme(item.dataset.theme)
 }
 
-button.addEventListener('click', async function () {
-  if (button.getAttribute('aria-expanded') === 'true') {
-    hideMenu()
-  } else {
-    showMenu()
-
-    const { x, y } = await computePosition(button, menu, {
-      placement: 'bottom',
-      middleware: [offset(8), shift({ padding: 16 })],
-    })
-
-    Object.assign(menu.style, {
-      left: `${x}px`,
-      top: `${y}px`,
-    })
-  }
-})
-
-let hideMenuOnBlurTimeout
-
-menu.addEventListener('blur', function () {
-  hideMenuOnBlurTimeout = setTimeout(hideMenu, 300)
-})
-
-menu.addEventListener('click', function () {
-  setMenuItemTheme(getActiveMenuItem())
-  clearTimeout(hideMenuOnBlurTimeout)
-})
-
-menuItems.forEach((item) =>
-  item.addEventListener('mouseover', function () {
-    focusMenuItem(item)
-  })
-)
-
-menu.addEventListener('keydown', function (e) {
-  const activeItem = getActiveMenuItem()
-
-  let nextActiveItem
-  let shouldPreventDefault
-
-  switch (e.key) {
-    case 'Escape':
-      hideMenu()
-      shouldPreventDefault = true
-      break
-    case 'Enter':
-    case ' ':
-      setMenuItemTheme(activeItem)
-      shouldPreventDefault = true
-      break
-    case 'ArrowDown':
-      nextActiveItem = activeItem.nextElementSibling || menu.firstElementChild
-      break
-    case 'ArrowUp':
-      nextActiveItem =
-        activeItem.previousElementSibling || menu.lastElementChild
-      break
-    case 'Home':
-    case 'PageUp':
-      nextActiveItem = menu.firstElementChild
-      break
-    case 'End':
-    case 'PageDown':
-      nextActiveItem = menu.lastElementChild
-      break
-  }
-
-  if (nextActiveItem) {
-    focusMenuItem(nextActiveItem)
-    shouldPreventDefault = true
-  }
-
-  if (shouldPreventDefault) {
-    e.preventDefault()
-  }
+initialize(button, {
+  onClick: setMenuItemTheme,
+  isSelectedItem: function (item) {
+    return item.dataset.theme === localStorage.getItem('__theme')
+  },
 })
 
 document.body.addEventListener('newTheme', function (e) {
-  function selectMenuItem(theme) {
-    const selectedStyle = ['font-bold', 'text-primary']
-
-    for (const item of menuItems) {
-      if (item.dataset.theme === theme) {
-        item.setAttribute('aria-selected', 'true')
-        item.classList.add(...selectedStyle)
-      } else {
-        item.removeAttribute('aria-selected')
-        item.classList.remove(...selectedStyle)
-      }
-    }
-  }
-
   function changeButtonIcon(theme) {
     const icon = {
       dark: '#moon',
@@ -177,5 +27,4 @@ document.body.addEventListener('newTheme', function (e) {
 
   const theme = e.detail.theme
   changeButtonIcon(theme)
-  selectMenuItem(theme)
 })

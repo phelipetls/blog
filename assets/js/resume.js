878f002369ca7190b181a8345c8a21629ef54691
Phelipe Teles <phelipe_teles@hotmail.com>
2022-05-08 17:18:23 -0300

    Don't use async await in resume script

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -15,37 +15,37 @@ const resumeUrl = resumeContainer.dataset.resume
 if (PDFObject.supportsPDFs) {
   PDFObject.embed(resumeUrl, resumeContainer)
 } else {
-  ;(async function () {
-    try {
-      resumeDownloadButton.classList.remove('hidden')
+  resumeDownloadButton.classList.remove('hidden')
 
-      const pdf = await pdfjsLib.getDocument(resumeUrl).promise
-      const page = await pdf.getPage(1)
+  pdfjsLib
+    .getDocument(resumeUrl)
+    .promise.then(function (pdf) {
+      pdf.getPage(1).then(function (page) {
+        const scale =
+          resumeContainer.clientWidth / page.getViewport({ scale: 1 }).width
+        const viewport = page.getViewport({ scale })
+        const outputScale = window.devicePixelRatio || 1
 
-      const scale =
-        resumeContainer.clientWidth / page.getViewport({ scale: 1 }).width
-      const viewport = page.getViewport({ scale })
-      const outputScale = window.devicePixelRatio || 1
+        const context = resumeCanvas.getContext('2d')
 
-      const context = resumeCanvas.getContext('2d')
+        resumeCanvas.width = Math.floor(viewport.width * outputScale)
+        resumeCanvas.height = Math.floor(viewport.height * outputScale)
+        resumeCanvas.style.width = Math.floor(viewport.width) + 'px'
+        resumeCanvas.style.height = Math.floor(viewport.height) + 'px'
 
-      resumeCanvas.width = Math.floor(viewport.width * outputScale)
-      resumeCanvas.height = Math.floor(viewport.height * outputScale)
-      resumeCanvas.style.width = Math.floor(viewport.width) + 'px'
-      resumeCanvas.style.height = Math.floor(viewport.height) + 'px'
+        const transform =
+          outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null
 
-      const transform =
-        outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null
+        const renderContext = {
+          canvasContext: context,
+          transform: transform,
+          viewport: viewport,
+        }
 
-      const renderContext = {
-        canvasContext: context,
-        transform: transform,
-        viewport: viewport,
-      }
-
-      page.render(renderContext)
-    } finally {
+        page.render(renderContext)
+      })
+    })
+    .then(function () {
       resumeLoading.classList.add('hidden')
-    }
-  })()
+    })
 }

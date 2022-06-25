3934ac11ab3cfa2174fecaa78f77b64954675f73
Phelipe Teles <phelipe_teles@hotmail.com>
2022-06-25 12:25:30 -0300

    Avoid depending on script to load to download PDF.js worker script

--- assets/js/resume-pdfjs.js
+++ assets/js/resume-pdfjs.js
@@ -7,52 +7,40 @@ const resumeContainer = document.querySelector('[data-resume]')
 const resumeUrl = resumeContainer.dataset.resume
 const resumeCanvas = document.querySelector('[data-resume-canvas]')
 const resumeLoading = document.querySelector('[data-resume-loading]')
-const resumeDownloadButton = document.querySelector(
-  '[data-resume-download-button]'
-)
 
-resumeLoading.classList.replace('hidden', 'flex')
-resumeDownloadButton.classList.replace('hidden', 'flex')
+pdfjsLib
+  .getDocument(resumeUrl)
+  .promise.then(function (pdf) {
+    pdf.getPage(1).then(function (page) {
+      const scale =
+        resumeContainer.clientWidth / page.getViewport({ scale: 1 }).width
+      const viewport = page.getViewport({ scale })
+      const outputScale = window.devicePixelRatio || 1
 
-const pdfWorkerScript = document.createElement('script')
-pdfWorkerScript.src = params.pdfjsWorkerUrl
-document.body.append(pdfWorkerScript)
+      const context = resumeCanvas.getContext('2d')
 
-pdfWorkerScript.onload = function () {
-  pdfjsLib
-    .getDocument(resumeUrl)
-    .promise.then(function (pdf) {
-      pdf.getPage(1).then(function (page) {
-        const scale =
-          resumeContainer.clientWidth / page.getViewport({ scale: 1 }).width
-        const viewport = page.getViewport({ scale })
-        const outputScale = window.devicePixelRatio || 1
+      resumeCanvas.width = Math.floor(viewport.width * outputScale)
+      resumeCanvas.height = Math.floor(viewport.height * outputScale)
+      resumeCanvas.style.width = Math.floor(viewport.width) + 'px'
+      resumeCanvas.style.height = Math.floor(viewport.height) + 'px'
 
-        const context = resumeCanvas.getContext('2d')
+      const transform =
+        outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null
 
-        resumeCanvas.width = Math.floor(viewport.width * outputScale)
-        resumeCanvas.height = Math.floor(viewport.height * outputScale)
-        resumeCanvas.style.width = Math.floor(viewport.width) + 'px'
-        resumeCanvas.style.height = Math.floor(viewport.height) + 'px'
+      const renderContext = {
+        canvasContext: context,
+        transform: transform,
+        viewport: viewport,
+      }
 
-        const transform =
-          outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null
-
-        const renderContext = {
-          canvasContext: context,
-          transform: transform,
-          viewport: viewport,
-        }
-
-        const renderTask = page.render(renderContext)
-        return renderTask.promise
-      })
+      const renderTask = page.render(renderContext)
+      return renderTask.promise
     })
-    .catch(function () {
-      resumeContainer.classList.add('hidden')
-    })
-    .then(function () {
-      resumeCanvas.classList.remove('hidden')
-      resumeLoading.classList.add('hidden')
-    })
-}
+  })
+  .catch(function () {
+    resumeContainer.classList.add('hidden')
+  })
+  .then(function () {
+    resumeCanvas.classList.remove('hidden')
+    resumeLoading.classList.add('hidden')
+  })
--- assets/js/resume.js
+++ assets/js/resume.js
@@ -4,9 +4,21 @@ import * as params from '@params'
 const resumeContainer = document.querySelector('[data-resume]')
 const resumeUrl = resumeContainer.dataset.resume
 
+const resumeLoading = document.querySelector('[data-resume-loading]')
+const resumeDownloadButton = document.querySelector(
+  '[data-resume-download-button]'
+)
+
 if (PDFObject.supportsPDFs) {
   PDFObject.embed(resumeUrl, resumeContainer)
 } else {
+  resumeLoading.classList.replace('hidden', 'flex')
+  resumeDownloadButton.classList.replace('hidden', 'flex')
+
+  const pdfWorkerScript = document.createElement('script')
+  pdfWorkerScript.src = params.pdfjsWorkerUrl
+  document.body.append(pdfWorkerScript)
+
   const resumePdfjsScript = document.createElement('script')
   resumePdfjsScript.src = params.resumePdfjsUrl
   document.body.append(resumePdfjsScript)

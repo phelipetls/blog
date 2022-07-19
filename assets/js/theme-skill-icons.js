9fd1ec97cbf3632b796cc28ed5a87f66fe5a6603
Phelipe Teles <phelipe_teles@hotmail.com>
2022-07-19 19:07:20 -0300

    Show three project icons for each project in homepage
    Handle automatically changing the CSS background images

--- assets/js/theme-skill-icons.js
+++ assets/js/theme-skill-icons.js
@@ -1,7 +1,35 @@
-document.body.addEventListener('newTheme', function (e) {
+// @ts-check
+
+/** @type {(rawUrl: string, theme: Theme) => string} */
+export function changeUrlTheme(rawUrl, theme) {
+  const url = new URL(rawUrl)
+  url.searchParams.set('theme', theme)
+  return url.toString()
+}
+
+/** @type {(elem: HTMLElement) => string} */
+function getUrlInCssBackgroundImage(elem) {
+  return elem.style.backgroundImage
+    .replace(/^url\(['"]/, '')
+    .replace(/['"]\)$/, '')
+}
+
+/** @type {(elem: HTMLElement, url: string) => void} */
+export function setCssBackgroundImageUrl(elem, url) {
+  elem.style.backgroundImage = `url('${url}')`
+}
+
+document.body.addEventListener('newTheme', function (/** @type {any} */ e) {
   document.querySelectorAll('img[src*=skillicons]').forEach(function (img) {
-    const url = new URL(img.getAttribute('src'))
-    url.searchParams.set('theme', e.detail.theme)
-    img.setAttribute('src', url)
+    const url = img.getAttribute('src')
+    img.setAttribute('src', changeUrlTheme(url, e.detail.theme))
   })
+
+  document
+    .querySelectorAll('[data-skill-icons-bg-image]')
+    .forEach(function (/** @type {HTMLElement} */ elem) {
+      const backgroundImageUrl = getUrlInCssBackgroundImage(elem)
+      const newUrl = changeUrlTheme(backgroundImageUrl, e.detail.theme)
+      setCssBackgroundImageUrl(elem, newUrl)
+    })
 })

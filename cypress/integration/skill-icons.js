077a6db41cf9fd9a150f17696601ab5589a9e8ea
Phelipe Teles <phelipe_teles@hotmail.com>
2022-07-19 23:01:04 -0300

    Test if img src changes when theme changes

---
+++ cypress/integration/skill-icons.js
@@ -0,0 +1,24 @@
+describe('Skill icons', () => {
+  ;['/', '/projects'].forEach((pageUrl) => {
+    describe(`page: ${pageUrl}`, () => {
+      it('should change skill icons URL when theme changes', () => {
+        cy.visit(pageUrl, {
+          onBeforeLoad(win) {
+            win.localStorage.setItem('__theme', 'light')
+          },
+        })
+
+        cy.get('img[src*=skillicons]').each(($img) => {
+          expect($img.attr('src')).to.match(/theme=light/)
+        })
+
+        cy.findByRole('button', { name: /change theme/i }).click()
+        cy.findByRole('option', { name: /dark/i }).click()
+
+        cy.get('img[src*=skillicons]').each(($img) => {
+          expect($img.attr('src')).to.match(/theme=dark/)
+        })
+      })
+    })
+  })
+})

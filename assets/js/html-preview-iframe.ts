6cb67b8aac31edc67d475baf043555ae72fa2bda
Phelipe Teles <phelipe_teles@hotmail.com>
2022-09-17 19:07:16 -0300

    Animate refresh icon while iframe is loading

--- assets/js/html-preview-iframe.ts
+++ assets/js/html-preview-iframe.ts
@@ -7,9 +7,16 @@ for (const htmlPreviewIframe of htmlPreviewIframeContainers) {
     '[data-html-preview-iframe-refresh]'
   )
 
+  const refreshIcon = refreshButton?.querySelector('svg')
+
   refreshButton?.addEventListener('click', () => {
     const iframe = htmlPreviewIframe.querySelector('iframe')
     iframe?.contentWindow?.location.reload()
+
+    refreshIcon?.classList.add('animate-spin')
+    iframe?.addEventListener('load', () => {
+      refreshIcon?.classList.remove('animate-spin')
+    })
   })
 }
 

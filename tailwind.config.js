a15d87f7ffc5e6b6b765c36ddf3bae4c4a11b8d5
Phelipe Teles <phelipe_teles@hotmail.com>
2022-10-01 12:07:54 -0300

    Use default shadow class for every shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         content: 'var(--content-max-width)',
       },
       boxShadow: {
-        t: '0 -1px 3px rgba(0, 0, 0, 0.5)',
+        t: '0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 -1px 2px -1px rgb(0 0 0 / 0.1)',
       },
       zIndex: {
         '-1': '-1',

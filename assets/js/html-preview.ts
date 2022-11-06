ff5a6aad9305721adce618adce195ef682479aef
Phelipe Teles <phelipe_teles@hotmail.com>
2022-11-06 16:53:37 -0300

    Refactor how to expand and collapse code block height

--- assets/js/html-preview.ts
+++ assets/js/html-preview.ts
@@ -1,10 +1,3 @@
-import * as params from '@params'
-
-declare module '@params' {
-  export const showLess: string
-  export const showMore: string
-}
-
 const expandCollapseButtons = document.querySelectorAll<HTMLElement>(
   '[data-html-expand-collapse-button]'
 )
@@ -14,62 +7,35 @@ for (const expandCollapseButton of expandCollapseButtons) {
     const codeBlockContainer = expandCollapseButton
       .closest('[data-html-preview]')
       ?.querySelector<HTMLElement>('[data-html-preview-codeblock-container]')
+
     if (!codeBlockContainer) {
       return
     }
 
     const codeBlock = codeBlockContainer.querySelector('pre')
-    if (codeBlock) {
-      codeBlock.classList.add('transition-[max-height]', 'duration-300', 'ease')
-    }
 
-    const chevronIcon = expandCollapseButton.querySelector('svg')
-    if (!chevronIcon) {
+    if (!codeBlock) {
       return
     }
 
-    chevronIcon.classList.add('transition-transform', 'duration-300', 'ease')
-
-    const changeButtonText = (text: string) => {
-      const textContainer = expandCollapseButton.querySelector('[data-text]')
-
-      if (textContainer) {
-        textContainer.textContent = text
-      }
-    }
-
-    const maxLinesOfCode = Number(codeBlockContainer.dataset.maxLinesOfCode)
-
-    const linesCount =
-      codeBlock?.querySelectorAll('.line').length || maxLinesOfCode
-
-    const isExpanded =
-      Number(codeBlockContainer.style.getPropertyValue('--is-expanded')) === 1
+    const isCollapsed = codeBlockContainer.dataset.isCollapsed === 'true'
 
     const expandCodeBlock = () => {
-      codeBlockContainer.style.setProperty(
-        '--lines-of-code',
-        String(linesCount)
-      )
-      codeBlockContainer.style.setProperty('--is-expanded', '1')
-      chevronIcon.classList.add('rotate-180')
-      changeButtonText(params.showLess)
+      const scrollbarHeight = codeBlock.scrollHeight - codeBlock.clientHeight
+      const maxHeight = codeBlock.scrollHeight + scrollbarHeight
+      codeBlock.style.maxHeight = `${maxHeight}px`
+      codeBlockContainer.setAttribute('data-is-collapsed', 'false')
     }
 
     const collapseCodeBlock = () => {
-      codeBlockContainer.style.setProperty('--is-expanded', '0')
-      codeBlockContainer.style.setProperty(
-        '--lines-of-code',
-        String(maxLinesOfCode)
-      )
-      chevronIcon.classList.remove('rotate-180')
-      changeButtonText(params.showMore)
+      codeBlock.style.maxHeight = ''
+      codeBlockContainer.setAttribute('data-is-collapsed', 'true')
     }
 
-    if (isExpanded) {
-      collapseCodeBlock()
-    } else {
+    if (isCollapsed) {
       expandCodeBlock()
+    } else {
+      collapseCodeBlock()
     }
   })
 }

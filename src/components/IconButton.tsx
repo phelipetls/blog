a5f907a90f0ff9f1473c2e02a4f303c16d5f29c1
Phelipe Teles <phelipe_teles@hotmail.com>
2023-01-07 17:56:55 -0300

    Proactively use tailwind-merge in IconButton className

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
 import Button, { ButtonProps } from './Button'
 
 export type IconButtonProps = ButtonProps & {
@@ -8,14 +9,16 @@ export type IconButtonProps = ButtonProps & {
 export default function IconButton(props: IconButtonProps) {
   const { className: className_, variant = 'rounded', ...rest } = props
 
-  const className = clsx(
-    'bg-background text-on-background',
-    {
-      ['rounded-full px-4 py-2']: variant === 'rounded-full',
-      ['rounded border border-divider p-1 shadow shadow-shadow']:
-        variant === 'rounded',
-    },
-    className_
+  const className = twMerge(
+    clsx(
+      'bg-background text-on-background',
+      {
+        ['rounded-full px-4 py-2']: variant === 'rounded-full',
+        ['rounded border border-divider p-1 shadow shadow-shadow']:
+          variant === 'rounded',
+      },
+      className_
+    )
   )
 
   return <Button className={className} {...rest} />

c2b86c4a4a7b18f25d1545601f155918718e71cf
Phelipe Teles <phelipe_teles@hotmail.com>
2023-01-07 17:55:51 -0300

    Fix IconButton icons being the same color as background

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -9,6 +9,7 @@ export default function IconButton(props: IconButtonProps) {
   const { className: className_, variant = 'rounded', ...rest } = props
 
   const className = clsx(
+    'bg-background text-on-background',
     {
       ['rounded-full px-4 py-2']: variant === 'rounded-full',
       ['rounded border border-divider p-1 shadow shadow-shadow']:

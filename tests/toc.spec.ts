aaaf659f0fd0a616b7483420c3aca58f33cdb3f5
Phelipe Teles <phelipe_teles@hotmail.com>
2023-04-07 19:59:24 -0300

    Upgrade playwright and use .toBeInViewport assertion

--- tests/toc.spec.ts
+++ tests/toc.spec.ts
@@ -1,24 +1,4 @@
-import { test, expect, Locator } from '@playwright/test'
-
-// TODO: remove this function once .viewportRatio or .toIntersectViewport
-// method comes into a stable Playwright release.
-// See https://github.com/microsoft/playwright/issues/8740
-function isIntersectingViewport(locator: Locator): Promise<boolean> {
-  return locator.evaluate(async (element) => {
-    const visibleRatio: number = await new Promise((resolve) => {
-      const observer = new IntersectionObserver((entries) => {
-        resolve(entries[0].intersectionRatio)
-        observer.disconnect()
-      })
-      observer.observe(element)
-      // Firefox doesn't call IntersectionObserver callback unless
-      // there are rafs.
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      requestAnimationFrame(() => {})
-    })
-    return visibleRatio > 0
-  })
-}
+import { test, expect } from '@playwright/test'
 
 test.describe('Table of contents interactive sidebar', () => {
   test('should highlight toc item when its corresponding heading comes into view', async ({
@@ -44,57 +24,32 @@ test.describe('Table of contents interactive sidebar', () => {
 
     // Check if conclusion link is not on viewport first, since it's the last
     // link in table of contents
-    await expect
-      .poll(
-        async () =>
-          await isIntersectingViewport(
-            page.locator('nav[data-toc]:visible a[href="#conclusion"]')
-          )
-      )
-      .toBeFalsy()
+    await expect(
+      page.locator('nav[data-toc]:visible a[href="#conclusion"]')
+    ).not.toBeInViewport()
 
     // Scroll to bottom of page and it should be visible now
     await page.keyboard.down('End')
-    await expect
-      .poll(
-        async () =>
-          await isIntersectingViewport(
-            page.locator('nav[data-toc]:visible a[href="#conclusion"]')
-          )
-      )
-      .toBeTruthy()
+    await expect(
+      page.locator('nav[data-toc]:visible a[href="#conclusion"]')
+    ).toBeInViewport()
 
     // Check if the same happens with an arbitrary heading
-    await expect
-      .poll(
-        async () =>
-          await isIntersectingViewport(
-            page.locator('nav[data-toc]:visible a[href="#redirections"]')
-          )
-      )
-      .toBeFalsy()
+    await expect(
+      page.locator('nav[data-toc]:visible a[href="#redirections"]')
+    ).not.toBeInViewport()
 
     await page
       .locator('h2', { hasText: 'Redirections' })
       .scrollIntoViewIfNeeded()
-    await expect
-      .poll(
-        async () =>
-          await isIntersectingViewport(
-            page.locator('nav[data-toc]:visible a[href="#redirections"]')
-          )
-      )
-      .toBeTruthy()
+    await expect(
+      page.locator('nav[data-toc]:visible a[href="#redirections"]')
+    ).toBeInViewport()
 
     // Scroll to top of page
     await page.keyboard.down('Home')
-    await expect
-      .poll(
-        async () =>
-          await isIntersectingViewport(
-            page.locator('nav[data-toc]:visible a[href="#hello-world"]')
-          )
-      )
-      .toBeTruthy()
+    await expect(
+      page.locator('nav[data-toc]:visible a[href="#hello-world"]')
+    ).toBeInViewport()
   })
 })

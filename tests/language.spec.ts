9bd2e5ff83a45358a0f2f66a30a6afe1741f8e42
Phelipe Teles <phelipe_teles@hotmail.com>
2023-04-21 20:34:58 -0300

    Add select to switch between languages

---
+++ tests/language.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Multi-language', () => {
+  const toPortugueseUrls = [
+    ['/', '/pt/'],
+    ['/posts', '/pt/posts'],
+  ]
+
+  toPortugueseUrls.forEach(([initial, expected]) => {
+    test(`should be able to switch to portuguese (from ${initial} to ${expected})`, async ({
+      page,
+    }) => {
+      await page.goto(initial)
+
+      await page
+        .getByLabel('Choose a language')
+        .selectOption({ label: 'Português' })
+
+      await expect(page).toHaveURL(expected)
+    })
+  })
+
+  const toEnglishUrls = [
+    ['/pt', '/'],
+    ['/pt/posts', '/posts'],
+  ]
+
+  toEnglishUrls.forEach(([initial, expected]) => {
+    test(`should be able to switch to english (from url ${initial} to ${expected})`, async ({
+      page,
+    }) => {
+      await page.goto(initial)
+
+      await page
+        .getByLabel('Escolha a linguagem')
+        .selectOption({ label: 'English' })
+
+      await expect(page).toHaveURL(expected)
+    })
+  })
+})

05269bb7692cb52c47a9d5e4a1121bb75be79bb7
Phelipe Teles <phelipe_teles@hotmail.com>
2023-06-14 21:32:12 -0300

    Reduce nesting in copy code tests

--- tests/copy-code.spec.ts
+++ tests/copy-code.spec.ts
@@ -15,49 +15,47 @@ test.describe('Copy code', () => {
     await expect(copyCodeButton).not.toHaveCSS('opacity', '0')
   })
 
-  test.describe('Clipboard', () => {
+  test.describe('With permission to write to clipboard', () => {
+    test.skip(({ browserName }) => browserName !== 'chromium')
+    test.use({ permissions: ['clipboard-write'] })
+
+    test('should copy code block contents on button click and show a tooltip indicating success', async ({
+      page,
+    }) => {
+      await page.goto('/posts/bash-for-javascript-developers')
+
+      const tooltip = page.getByRole('tooltip', { name: 'Copied' })
+
+      await expect(tooltip).not.toBeVisible()
+
+      await page.locator('pre').first().hover()
+      await page
+        .getByRole('button', { name: /copy code/i })
+        .first()
+        .click()
+
+      await expect(tooltip).toBeVisible()
+      await expect(tooltip).not.toBeVisible()
+    })
+  })
+
+  test.describe('Without permission to write to clipboard', () => {
     test.skip(({ browserName }) => browserName !== 'chromium')
 
-    test.describe('With permission to write to clipboard', () => {
-      test.use({ permissions: ['clipboard-write'] })
+    test('should show tooltip indicating the error', async ({ page }) => {
+      await page.goto('/posts/bash-for-javascript-developers')
 
-      test('should copy code block contents on button click and show a tooltip indicating success', async ({
-        page,
-      }) => {
-        await page.goto('/posts/bash-for-javascript-developers')
+      await page.locator('pre').first().hover()
+      await page
+        .getByRole('button', { name: /copy code/i })
+        .first()
+        .click()
 
-        const tooltip = page.getByRole('tooltip', { name: 'Copied' })
+      await expect(page.getByRole('tooltip')).toBeVisible()
+      await expect(page.getByRole('tooltip')).toHaveText('Failed to copy')
 
-        await expect(tooltip).not.toBeVisible()
-
-        await page.locator('pre').first().hover()
-        await page
-          .getByRole('button', { name: /copy code/i })
-          .first()
-          .click()
-
-        await expect(tooltip).toBeVisible()
-        await expect(tooltip).not.toBeVisible()
-      })
-    })
-
-    test.describe('Without permission to write to clipboard', () => {
-      test('should show tooltip indicating the error', async ({ page }) => {
-        await page.goto('/posts/bash-for-javascript-developers')
-
-        const tooltip = page.getByRole('tooltip', { name: 'Failed to copy' })
-
-        await expect(tooltip).not.toBeVisible()
-
-        await page.locator('pre').first().hover()
-        await page
-          .getByRole('button', { name: /copy code/i })
-          .first()
-          .click()
-
-        await expect(tooltip).toBeVisible()
-        await expect(tooltip).not.toBeVisible()
-      })
+      await expect(page.getByRole('tooltip')).toBeVisible()
+      await expect(page.getByRole('tooltip')).not.toBeVisible()
     })
   })
 })

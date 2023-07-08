3d4e47e34b0373b1dc560e64f7c5f7d4b888709f
Phelipe Teles <phelipe_teles@hotmail.com>
2023-07-08 20:09:11 -0300

    Filter out draft post in prod only

--- src/utils/posts/getBlogPosts.ts
+++ src/utils/posts/getBlogPosts.ts
@@ -1,14 +1,30 @@
 import type { Language } from '@utils/i18n'
 import { getCollection } from 'astro:content'
 
+const isProd = !import.meta.env.DEV
+
 export function getBlogPosts(language: Language) {
   if (language === 'pt') {
     return getCollection('posts', (blogPost) => {
-      return blogPost.id.endsWith('.pt.mdx') && !blogPost.data.draft
+      const isPortuguese = blogPost.id.endsWith('.pt.mdx')
+      const isDraft = blogPost.data.draft
+
+      if (isProd) {
+        return isPortuguese && !isDraft
+      } else {
+        return isPortuguese
+      }
     })
   }
 
   return getCollection('posts', (blogPost) => {
-    return !blogPost.id.endsWith('.pt.mdx') && !blogPost.data.draft
+    const isEnglish = !blogPost.id.endsWith('.pt.mdx')
+    const isDraft = blogPost.data.draft
+
+    if (isProd) {
+      return isEnglish && !isDraft
+    } else {
+      return isEnglish
+    }
   })
 }

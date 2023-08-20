27b9cedd723ba716ee1189f26972b0a496a90c37
Phelipe Teles <phelipe_teles@hotmail.com>
2023-08-20 12:07:53 -0300

    Use surface color as Button background

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -49,7 +49,7 @@ export const Button = React.forwardRef<HTMLElement, ButtonProps>(
           {
             ['bg-primary text-on-primary hover:bg-primary-hover shadow-sm shadow-shadow']:
               color === 'primary',
-            ['bg-background text-on-background']: color === 'secondary',
+            ['bg-surface text-on-background']: color === 'secondary',
           },
           className
         )

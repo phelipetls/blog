bbaea9892d8eea81764954d0b82c0947590df6b8
Phelipe Teles <phelipe_teles@hotmail.com>
2023-10-06 20:25:02 -0300

    Make Button component polymorphic

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,105 +9,64 @@ type CommonButtonProps = {
   size?: 'normal' | 'huge'
 }
 
-type ButtonLinkProps = Omit<
-  React.ComponentPropsWithRef<'a'>,
-  'href' & keyof CommonButtonProps
-> & {
-  as?: never
-  href: string
-}
+type PropsOf<T extends React.ElementType> = React.ComponentPropsWithRef<T>
 
-type ButtonButtonProps = Omit<
-  React.ComponentPropsWithRef<'button'>,
-  keyof CommonButtonProps
-> & {
-  as?: never
-  href?: never
-}
+type PolymorphicRef<T extends React.ElementType> =
+  React.ComponentPropsWithRef<T>['ref']
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type CustomButtonProps = any
+type PolymorphicProps<
+  T extends React.ElementType = React.ElementType,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  TProps = {}
+> = {
+  as?: T
+} & TProps &
+  Omit<PropsOf<T>, keyof TProps | 'as' | 'ref'> & { ref?: PolymorphicRef<T> }
 
-export type ButtonProps = CommonButtonProps &
-  (ButtonLinkProps | ButtonButtonProps | CustomButtonProps)
+export type ButtonProps<T extends React.ElementType = 'button'> =
+  PolymorphicProps<T, CommonButtonProps>
 
-export const Button = React.forwardRef<HTMLElement, ButtonProps>(
-  (props, ref) => {
-    const mergedClassName = useMemo(() => {
-      const { color = 'primary', size = 'normal', className } = props
+type ButtonComponent = <T extends React.ElementType = 'button'>(
+  props: PolymorphicProps<T, ButtonProps<T>>
+) => React.ReactElement | null
 
-      const merged = twMerge(
-        clsx(
-          'flex',
-          'items-center',
-          'justify-center',
-          'flex-row',
-          'gap-2',
-          'transition-colors',
-          'duration-300',
-          'hover:bg-hover',
-          'disabled:bg-disabled',
-          'disabled:text-on-disabled',
-          'disabled:hover:cursor-not-allowed',
-          'disabled:hover:bg-disabled',
-          size === 'normal' ? 'rounded py-1 px-2' : 'px-4 py-2 rounded-full',
-          {
-            ['bg-primary text-on-primary hover:bg-primary-hover shadow-sm shadow-shadow']:
-              color === 'primary',
-            ['bg-surface text-on-background']: color === 'secondary',
-          },
-          className
-        )
+export const Button: ButtonComponent = React.forwardRef(function InnerButton<
+  T extends React.ElementType = 'button'
+>(props: ButtonProps<T>, ref: PolymorphicRef<T>) {
+  const mergedClassName = useMemo(() => {
+    const { color = 'primary', size = 'normal', className } = props
+
+    const merged = twMerge(
+      clsx(
+        'flex',
+        'items-center',
+        'justify-center',
+        'flex-row',
+        'gap-2',
+        'transition-colors',
+        'duration-300',
+        'hover:bg-hover',
+        'disabled:bg-disabled',
+        'disabled:text-on-disabled',
+        'disabled:hover:cursor-not-allowed',
+        'disabled:hover:bg-disabled',
+        size === 'normal' ? 'rounded py-1 px-2' : 'px-4 py-2 rounded-full',
+        {
+          ['bg-primary text-on-primary hover:bg-primary-hover shadow-sm shadow-shadow']:
+            color === 'primary',
+          ['bg-surface text-on-background']: color === 'secondary',
+        },
+        className
       )
-
-      return merged
-    }, [props.className, props.color])
-
-    if (props.href !== undefined) {
-      if (props.disabled) {
-        return (
-          <button
-            ref={ref as React.ForwardedRef<HTMLButtonElement>}
-            disabled={props.disabled}
-            className={mergedClassName}
-          >
-            {props.children}
-          </button>
-        )
-      }
-
-      const { children, size: _, color: __, ...rest } = props
-
-      return (
-        <a
-          ref={ref as React.ForwardedRef<HTMLAnchorElement>}
-          {...rest}
-          className={mergedClassName}
-        >
-          {children}
-        </a>
-      )
-    }
-
-    if (props.as) {
-      const { size: _, color: __, ...rest } = props
-      const Component = props.as
-
-      return <Component ref={ref} {...rest} className={mergedClassName} />
-    }
-
-    const { size: _, color: __, ...rest } = props
-
-    return (
-      <button
-        ref={ref as React.ForwardedRef<HTMLButtonElement>}
-        {...rest}
-        className={mergedClassName}
-      />
     )
-  }
-)
 
-Button.displayName = 'Button'
+    return merged
+  }, [props.className, props.color])
+
+  const { size: _, color: __, ...rest } = props
+  const Component = props.as ?? 'button'
+
+  return <Component ref={ref} {...rest} className={mergedClassName} />
+})
 
 export default Button

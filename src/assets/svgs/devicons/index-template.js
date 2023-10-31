546eeb811638ee271078bdab600aaa2d6a16baf7
Phelipe Teles <phelipe_teles@hotmail.com>
2023-10-31 12:48:49 -0300

    Upgrade @svgr/cli to latest version

--- src/assets/svgs/devicons/index-template.js
+++ src/assets/svgs/devicons/index-template.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 function defaultIndexTemplate(filePaths) {
-  const exportEntries = filePaths.map((filePath) => {
+  const exportEntries = filePaths.map(({ path: filePath }) => {
     const basename = path.basename(filePath, path.extname(filePath))
     const exportName = basename
     return `export { default as ${exportName} } from './${basename}'`

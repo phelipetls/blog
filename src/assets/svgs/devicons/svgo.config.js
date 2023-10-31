2ef0b1f8c3fcb50895fc52bdd4afa5a7238f5f59
Phelipe Teles <phelipe_teles@hotmail.com>
2023-10-31 08:41:18 -0300

    Use colorful devicons and correct some low contrast colors

--- src/assets/svgs/devicons/svgo.config.js
+++ src/assets/svgs/devicons/svgo.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  plugins: [
-    'preset-default',
-
-    {
-      name: 'convertColors',
-      params: {
-        currentColor: true,
-      },
-    },
-  ],
+  plugins: ['preset-default'],
 }

ad67ecd13a1344861ca5a18be8e1b4bb69f80b84
Phelipe Teles <phelipe_teles@hotmail.com>
2023-11-05 10:09:39 -0300

    Refactor out Sandpack console into a component

--- src/components/Sandpack.tsx
+++ src/components/Sandpack.tsx
@@ -22,6 +22,7 @@ import clsx from 'clsx'
 import CopyCodeBlockButton from './CopyCodeBlockButton'
 import Button from './Button'
 import IconButton from './IconButton'
+import { SandpackConsole } from './SandpackConsole'
 
 export type SandpackProps = SandpackProviderProps & {
   title: string
@@ -334,79 +335,7 @@ function CustomSandpack(props: CustomSandpackProps) {
               {emptyLogs ? (
                 <div className='px-horizontal-padding'>No logs yet</div>
               ) : (
-                <>
-                  {logs
-                    .filter((log) => log.data?.some((line) => line !== ''))
-                    .reduce((logs, log) => {
-                      const lastLog = logs.at(-1)
-
-                      if (!lastLog) {
-                        logs.push({ ...log, count: 1 })
-                        return logs
-                      }
-
-                      function areLogEntriesEqual(
-                        logEntryA: typeof log,
-                        logEntryB: typeof log
-                      ) {
-                        return (
-                          logEntryA.method === logEntryB.method &&
-                          logEntryA.data?.length === logEntryB.data?.length &&
-                          logEntryA.data?.every(
-                            (data, index) =>
-                              JSON.stringify(data) ===
-                              JSON.stringify(logEntryB.data?.[index])
-                          )
-                        )
-                      }
-
-                      if (areLogEntriesEqual(log, lastLog)) {
-                        logs[logs.length - 1] = {
-                          ...log,
-                          count: lastLog.count + 1,
-                        }
-                        return logs
-                      }
-
-                      logs.push({
-                        ...log,
-                        count: 1,
-                      })
-                      return logs
-                    }, [] as ((typeof logs)[0] & { count: number })[])
-                    .map((log) => {
-                      return (
-                        <div
-                          key={log.id}
-                          className={clsx(
-                            'whitespace-nowrap border-l-2 px-horizontal-padding py-2 [overflow-anchor:none]',
-                            log.method === 'error'
-                              ? 'border-warn'
-                              : 'border-note'
-                          )}
-                        >
-                          {log.data
-                            ?.map((d) => {
-                              if (typeof d === 'object' && '@t' in d) {
-                                return d['@t']
-                                  .replace(/^\[\[/, '')
-                                  .replace(/\]\]$/, '')
-                              }
-
-                              return JSON.stringify(d, null, 2)
-                            })
-                            .join(' ')}
-                          {log.count > 1 && (
-                            <span className='ml-2 aspect-square w-2 rounded-full bg-surface px-2 py-1 text-on-background'>
-                              {log.count}
-                            </span>
-                          )}
-                        </div>
-                      )
-                    })}
-
-                  <div className='h-[1px] [overflow-anchor:auto]' />
-                </>
+                <SandpackConsole logs={logs} />
               )}
             </div>
 
---
+++ src/components/SandpackConsole.tsx
@@ -0,0 +1,80 @@
+import type { SandpackConsoleData } from '@codesandbox/sandpack-react/components/Console/utils/getType'
+import clsx from 'clsx'
+
+type SandpackConsoleProps = {
+  logs: SandpackConsoleData
+}
+
+export function SandpackConsole(props: SandpackConsoleProps) {
+  return (
+    <>
+      {props.logs
+        .filter((log) => log.data?.some((line) => line !== ''))
+        .reduce((logs, log) => {
+          const lastLog = logs.at(-1)
+
+          if (!lastLog) {
+            logs.push({ ...log, count: 1 })
+            return logs
+          }
+
+          function areLogEntriesEqual(
+            logEntryA: typeof log,
+            logEntryB: typeof log
+          ) {
+            return (
+              logEntryA.method === logEntryB.method &&
+              logEntryA.data?.length === logEntryB.data?.length &&
+              logEntryA.data?.every(
+                (data, index) =>
+                  JSON.stringify(data) ===
+                  JSON.stringify(logEntryB.data?.[index])
+              )
+            )
+          }
+
+          if (areLogEntriesEqual(log, lastLog)) {
+            logs[logs.length - 1] = {
+              ...log,
+              count: lastLog.count + 1,
+            }
+            return logs
+          }
+
+          logs.push({
+            ...log,
+            count: 1,
+          })
+          return logs
+        }, [] as ((typeof props.logs)[0] & { count: number })[])
+        .map((log) => {
+          return (
+            <div
+              key={log.id}
+              className={clsx(
+                'whitespace-nowrap border-l-2 px-horizontal-padding py-2 [overflow-anchor:none]',
+                log.method === 'error' ? 'border-warn' : 'border-note'
+              )}
+            >
+              {log.data
+                ?.map((d) => {
+                  if (typeof d === 'object' && '@t' in d) {
+                    return d['@t'].replace(/^\[\[/, '').replace(/\]\]$/, '')
+                  }
+
+                  return JSON.stringify(d, null, 2)
+                })
+                .join(' ')}
+              {log.count > 1 && (
+                <span className='ml-2 aspect-square w-2 rounded-full bg-surface px-2 py-1 text-on-background'>
+                  {log.count}
+                </span>
+              )}
+            </div>
+          )
+        })}
+
+      <div className='h-[1px] [overflow-anchor:auto]' />
+    </>
+  )
+}

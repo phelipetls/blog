082f647a93d117da9a3fb1b2d7e6425e3fe3a535
Phelipe Teles <phelipe_teles@hotmail.com>
2023-11-11 18:31:54 -0300

    Rename test file

--- tests/i18n.spec.ts
+++ tests/i18n.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Multi-language', () => {
+test.describe('Internationalization', () => {
   const toPortugueseUrls = [
     ['/', '/pt/'],
     ['/posts', '/pt/posts'],

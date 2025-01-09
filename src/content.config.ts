4d4c308adc428cc7dea9e9bc8dc4f00f75a54a67
Phelipe Teles <phelipe_teles@hotmail.com>
2025-01-09 12:00:02 -0300

    Migrate to new Astro loaders to define collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
+import { glob } from 'astro/loaders'
 import { z, defineCollection } from 'astro:content'
 
 const postsCollection = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/content/posts' }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
--- src/utils/posts/getBlogPostsAsRssItems.ts
+++ src/utils/posts/getBlogPostsAsRssItems.ts
@@ -1,13 +1,15 @@
 import { getRelativeLocaleUrl } from 'astro:i18n'
 import { getBlogPosts } from './getBlogPosts'
 import type { Locale } from '@utils/i18n/locales'
+import { render } from 'astro:content'
+import { getBlogPostFolderName } from './getBlogPostFolderName'
 
 export async function getBlogPostsAsRssItems(locale: Locale) {
   const blogPosts = await getBlogPosts(locale)
 
   return await Promise.all(
     blogPosts.map(async (blogPost) => {
-      const { remarkPluginFrontmatter } = await blogPost.render()
+      const { remarkPluginFrontmatter } = await render(blogPost)
 
       if (typeof remarkPluginFrontmatter.summary !== 'string') {
         throw new Error(
@@ -21,7 +23,10 @@ export async function getBlogPostsAsRssItems(locale: Locale) {
         title: blogPost.data.title,
         pubDate: blogPost.data.date,
         description: remarkPluginFrontmatter.summary,
-        link: getRelativeLocaleUrl('pt', `/posts/${blogPost.slug}`),
+        link: getRelativeLocaleUrl(
+          'pt',
+          `/posts/${getBlogPostFolderName(blogPost)}`
+        ),
       }
     })
   )

e72e6b82ec970da5150190f025d1576366e98c89
Phelipe Teles <phelipe_teles@hotmail.com>
2025-09-26 13:57:22 -0300

    Add snapshot tests to code blocks

---
+++ tests/code-blocks-snapshots.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Code Block Snapshots', () => {
+  test('simple codeblock with highlighted lines snapshot', async ({ page, browserName }) => {
+    test.skip(browserName !== 'chromium')
+
+    await page.goto('/posts/debouncing-react')
+
+    const highlightedCodeBlock = page
+      .locator('[data-codeblock]')
+      .first()
+
+    await expect(highlightedCodeBlock).toHaveScreenshot('code-block-highlighted-lines.png')
+  })
+
+  test('simple codeblock with title', async ({ page, browserName }) => {
+    test.skip(browserName !== 'chromium')
+
+    await page.goto('/posts/using-storybook-and-msw-in-react-native')
+
+    const codeBlockWithTitle = page
+      .locator('[data-codeblock]')
+      .first()
+
+    await expect(codeBlockWithTitle).toHaveScreenshot('code-block-with-title.png')
+  })
+
+  test('twoslash code block with highlighted lines snapshot', async ({ page, browserName }) => {
+    test.skip(browserName !== 'chromium')
+
+    await page.goto('/posts/polymorphic-components-react-typescript')
+
+    const twoslashCodeBlock = page
+      .locator('[data-codeblock]')
+      .nth(5)
+
+    await expect(twoslashCodeBlock).toHaveScreenshot('twoslash-code-block.png')
+  })
+})
